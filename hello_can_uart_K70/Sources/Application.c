df9a7514ff85544ad8998bf50ae616db44fb7912
Alejandro <perez.alejandrodaniel@gmail.com>
2016-07-09 16:49:37 -0300

    Remove a unused component

--- hello_can_uart_K70/Sources/Application.c
+++ hello_can_uart_K70/Sources/Application.c
@@ -7,17 +7,20 @@ static CAN_Desc canData;
 volatile bool DataFrameTxFlg;
 volatile bool DataFrameRxFlg = FALSE;
 
-uint8_t rxbuffer; /* Initialization of output data buffer */
-uint8_t txbuffer;
 LDD_TError Error;
 LDD_CAN_TFrame Frame;
 
 static void SendChar(unsigned char ch, UART_Desc *desc) {
-	desc->isSent = FALSE; /* this will be set to 1 once the block has been sent */
+	/* this will be set to 1 once the block has been sent */
+	desc->isSent = FALSE;
+
+	/* Send char */
 	while (AS1_SendBlock(desc->handle, (LDD_TData*) &ch, 1) != ERR_OK) {
-	} /* Send char */
+	}
+
+	/* wait until we get the green flag from the TX UART interrupt */
 	while (!desc->isSent) {
-	} /* wait until we get the green flag from the TX interrupt */
+	}
 }
 
 static void SendString(const unsigned char *str, UART_Desc *desc) {
@@ -27,20 +30,21 @@ static void SendString(const unsigned char *str, UART_Desc *desc) {
 }
 
 static void Init(void) {
-	// Inicializacion UART
+	// UART Initialization
 	uartData.handle = AS1_Init(&uartData);
 	uartData.isSent = FALSE;
 	uartData.rxChar = '\0';
 	uartData.rxPutFct = UART_RxBuff_Put;
 
+	// CAN Initialization
 	canData.handle = CAN1_Init(&canData);
 	canData.isSent = FALSE;
 	canData.rxChar = '\0';
 	canData.rxPutFct = CAN_RxBuff_Put;
 
 	/* set up to receive RX into input buffer */
-	UART_RxBuff_Init(); /* initialize RX buffer */
-	CAN_RxBuff_Init(); /* initialize RX buffer */
+	UART_RxBuff_Init(); /* initialize RX buffer assigned to the UART module */
+	CAN_RxBuff_Init(); 	/* initialize RX buffer assigned to the CAN moduel */
 
 	/* Set up ReceiveBlock() with a single byte buffer. We will be called in OnBlockReceived() event. */
 	while (AS1_ReceiveBlock(uartData.handle, (LDD_TData *) &uartData.rxChar,
@@ -56,19 +60,19 @@ void APP_Run(void) {
 
 	for (;;) {
 		if (UART_RxBuff_NofElements() != 0) {
-			int i = 0;
+			uint8_t txbuffer;
 			while (UART_RxBuff_NofElements() != 0) {
 				unsigned char ch;
 
+				// Rx Receives a character from the console.
 				(void) UART_RxBuff_Get(&ch);
 				txbuffer = ch;
 
-				// Rx --Recibe un caracter desde consola--
-				// prepare frame
-				Frame.MessageID = 0x123U; /* Set Tx ID value - standard */
-				Frame.FrameType = LDD_CAN_DATA_FRAME; /* Specyfying type of Tx frame - Data frame */
-				Frame.Length = sizeof(txbuffer); /* Set number of bytes in data frame - 4B */
-				Frame.Data = &txbuffer; /* Set pointer to OutData buffer */
+				// Prepare frame
+				Frame.MessageID = 0x123U; 				/* Set Tx ID value - standard */
+				Frame.FrameType = LDD_CAN_DATA_FRAME; 	/* Specyfying type of Tx frame - Data frame */
+				Frame.Length = sizeof(txbuffer); 		/* Set number of bytes in data frame - 4B */
+				Frame.Data = &txbuffer; 				/* Set pointer to OutData buffer */
 
 				canData.isSent = FALSE;
 				Error = CAN1_SendFrame(canData.handle, 1U, &Frame); /* Sends the data frame over buffer 0 */
@@ -76,15 +80,18 @@ void APP_Run(void) {
 					break;   // break to error state
 				}
 
+				// Wait the completion of the transmission.
 				while (!canData.isSent);
 			}
 
 
 			while (CAN_RxBuff_NofElements()) {
 				unsigned char ch;
+
 				//receive frame (buffer 0)
 				CAN_RxBuff_Get(&ch);
 
+				// Send the echo to the console.
 				SendString((unsigned char*) "echo: ", &uartData);
 				SendChar(ch, &uartData);
 				SendString((unsigned char*) "\r\n", &uartData);
@@ -94,7 +101,7 @@ void APP_Run(void) {
 		GREENLED_NegVal(GREENLED_DeviceData);
 		// Wait counter reset
 		TU1_ResetCounter(TU1_DeviceData);
-		// wait 500 ticks (aaprox 0.5s)
+		// wait 500 ticks (~ 0.5s)
 		while (TU1_GetCounterValue(TU1_DeviceData) < 500) {
 		};
 
