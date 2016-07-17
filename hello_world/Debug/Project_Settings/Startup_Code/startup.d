02efbf4a922f5c1de0fffa30e79768ca366e6fb5
Alejandro <perez.alejandrodaniel@gmail.com>
2016-07-17 02:32:08 -0300

    Se borran archivos generados que son innecesarios

--- hello_world/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /home/alejandro/workspace.kds.k70/hello_world/Generated_Code/IO_Map.h \
- /home/alejandro/workspace.kds.k70/hello_world/Static_Code/IO_Map/MK70F12.h
-
-/home/alejandro/workspace.kds.k70/hello_world/Generated_Code/IO_Map.h:
-
-/home/alejandro/workspace.kds.k70/hello_world/Static_Code/IO_Map/MK70F12.h:
